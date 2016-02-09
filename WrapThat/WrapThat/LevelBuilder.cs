20f3434e1557373178c26c2365a4b1ca5114ae09
claus jensen <clau3028@student.eadania.dk>
2016-02-09 10:59:03 +0100

    try

---
+++ WrapThat/WrapThat/LevelBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace WrapThat
+{
+    class LevelBuilder : IBuilder
+    {
+
+        private GameObject gameObject;
+        public GameObject GetResult()
+        {
+            return gameObject;
+        }
+
+        public void BuildGameObject(Vector2 position)
+        {
+            GameObject gameObject = new GameObject();
+            gameObject.AddComponent(new SpriteRenderer(gameObject, "tile", 1f));
+            gameObject.Transform.Position = new Vector2(10, 10);
+            gameObject.AddComponent(new Animator(gameObject));
+            gameObject.AddComponent(new Player(gameObject));
+            //gameObject.AddComponent(new Collider());
+
+            this.gameObject = gameObject;
+        }
+    }
+}
