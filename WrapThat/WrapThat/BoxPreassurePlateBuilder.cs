e186261fd655c52c816d432e935749f4e0e88bc6
NielsJustesen <niels449@hotmail.com>
2016-02-12 10:42:55 +0100

    Preassureplate test

--- WrapThat/WrapThat/BoxPreassurePlateBuilder.cs
+++ WrapThat/WrapThat/BoxPreassurePlateBuilder.cs
@@ -13,7 +13,7 @@ namespace WrapThat
         {
             GameObject gameObject = new GameObject();
             gameObject.AddComponent(new SpriteRenderer(gameObject, "BoxPreassurePlate", 1f));
-            gameObject.Transform.Position = new Vector2(10, 10);
+            gameObject.Transform.Position = new Vector2(200, 200);
             gameObject.AddComponent(new Animator(gameObject));
             gameObject.AddComponent(new BoxPreassurePlate(gameObject, Color.White));
             gameObject.AddComponent(new Collider(gameObject));
